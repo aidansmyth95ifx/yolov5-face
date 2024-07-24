c29bfb2d3ace7b4e05ed44318c28dcd383c808e8
Aidan Smyth <Aidan.Smyth2@infineon.com>
2024-07-23 18:49:28 -0700

    Updating to match main CV repo

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -4,9 +4,10 @@
 #    $ bash weights/download_weights.sh
 
 python3 - <<EOF
+#/home/mtb_cv/virtualenvs/yolov5face_venv/bin/python - <<EOF
 from utils.google_utils import attempt_download
 
-for x in ['s', 'm', 'l', 'x']:
+for x in ['s', 'm', 'l', 'x', 'n']:
     attempt_download(f'yolov5{x}.pt')
 
 EOF
